37f6b36fa7aa0bad83b5bc2f677689d9ea8ec26f
Masen Furer <m_github@0x26.net>
2022-05-24 16:17:37 -0700

    remove unused DATA_TYPE_MAP
    prefer aprs3.classes.DataType instead

--- aprs3/constants.py
+++ aprs3/constants.py
@@ -25,15 +25,6 @@ APRSIS_URL = os.environ.get("APRSIS_URL", b"http://srvr.aprs-is.net:8080")
 
 DEFAULT_TOCALL = b"APZ069"
 
-DATA_TYPE_MAP = {
-    b">": b"status",
-    b"!": b"position_nots_nomsg",
-    b"=": b"position_nots_msg",
-    b"T": b"telemetry",
-    b";": b"object",
-    b"`": b"old_mice",
-}
-
 
 class TimestampFormat(enum.Enum):
     DayHoursMinutesLocal = b"/"
