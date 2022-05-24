46cc496fa34b2538f954add20931c35ee544527f
Masen Furer <m_github@0x26.net>
2022-05-24 16:17:52 -0700

    __init__.py: expose public API

--- aprs3/__init__.py
+++ aprs3/__init__.py
@@ -14,7 +14,19 @@ Python APRS Module.
 :source: <https://github.com/ampledata/aprs>
 
 """
+from importlib_metadata import version
 
+from . import decimaldegrees, geo_util, position, timestamp
+from .classes import (
+    DataType,
+    DataTypeError,
+    InformationField,
+    ItemReport,
+    Message,
+    ObjectReport,
+    PositionReport,
+    StatusReport,
+)
 from .constants import (
     APRSIS_HTTP_HEADERS,
     APRSIS_SERVERS,
@@ -22,15 +34,61 @@ from .constants import (
     APRSIS_RX_PORT,
     APRSIS_URL,
     DEFAULT_TOCALL,
-    DATA_TYPE_MAP,
+    PositionFormat,
+    TimestampFormat,
+    timestamp_formats_map,
 )
+from .data_ext import (
+    AreaObject,
+    CourseSpeed,
+    DataExt,
+    DFS,
+    PHG,
+    RNG,
+)
+from .position import Position
+from .timestamp import Timestamp
 
-from .geo_util import dec2dm_lat, dec2dm_lng, ambiguate  # NOQA
-
-from .classes import InformationField, PositionReport
-
-__author__ = "Greg Albrecht W2GMD <oss@undef.net>"  # NOQA pylint: disable=R0801
+__author__ = "Greg Albrecht W2GMD <oss@undef.net>"
 __copyright__ = (
-    "Copyright 2017 Greg Albrecht and Contributors"  # NOQA pylint: disable=R0801
+    "Copyright 2017 Greg Albrecht and Contributors"
 )
-__license__ = "Apache License, Version 2.0"  # NOQA pylint: disable=R0801
+__license__ = "Apache License, Version 2.0"
+__distribution = "aprs3"
+__version__ = version(__distribution)
+__all__ = [
+    "APRSIS_HTTP_HEADERS",
+    "APRSIS_SERVERS",
+    "APRSIS_FILTER_PORT",
+    "APRSIS_RX_PORT",
+    "APRSIS_URL",
+    "AreaObject",
+    "CourseSpeed",
+    "DataExt",
+    "DataType",
+    "DataTypeError",
+    "decimaldegrees",
+    "DEFAULT_TOCALL",
+    "DFS",
+    "geo_util",
+    "ItemReport",
+    "InformationField",
+    "Message",
+    "ObjectReport",
+    "PHG",
+    "position",
+    "Position",
+    "PositionFormat",
+    "PositionReport",
+    "RNG",
+    "StatusReport",
+    "timestamp",
+    "Timestamp",
+    "TimestampFormat",
+    "timestamp_formats_map",
+    "__author__",
+    "__copyright__",
+    "__license__",
+    "__distribution__",
+    "__version__",
+]
