5a9529b71dcb621903769464810597dae6306f75
Masen Furer <m_github@0x26.net>
2022-05-25 14:59:18 -0700

    examples/tcp_kiss_send_recv.py: simple example
    send a frame and then receive until KeyboardInterrupt

---
+++ examples/tcp_kiss_send_recv.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import os
+
+import aprs3
+
+
+MYCALL = os.environ.get("MYCALL", "N0CALL")
+KISS_HOST = os.environ.get("KISS_HOST", "localhost")
+KISS_PORT = os.environ.get("KISS_PORT", "8001")
+
+
+def main():
+    ki = aprs3.TCPKISS(host=KISS_HOST, port=int(KISS_PORT))
+    ki.start()
+    frame = aprs3.APRSFrame.ui(
+        destination="APZ001",
+        source=MYCALL,
+        path=["WIDE1-1"],
+        info=b">Hello World!",
+    )
+    ki.write(frame)
+    while True:
+        for frame in ki.read(min_frames=1):
+            print(repr(frame))
+
+
+if __name__ == "__main__":
+    main()
