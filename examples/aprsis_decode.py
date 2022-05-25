11de988398e33a69c118d6e9b1e5cabea9c1c64d
Masen Furer <m_github@0x26.net>
2022-05-24 20:34:20 -0700

    examples/aprsis_decode: simplify example

--- examples/aprsis_decode.py
+++ examples/aprsis_decode.py
@@ -1,8 +1,7 @@
 import asyncio
 import os
 
-from aprs3 import InformationField
-from kiss3 import tnc2
+from aprs3 import APRSFrame, create_aprsis_connection, InformationField
 
 MYCALL = os.environ.get("MYCALL", "N0CALL")
 DESTINATION = os.environ.get("DESTINATION", "APZ069")
@@ -11,33 +10,29 @@ PORT = int(os.environ.get("APRSIS_PORT", "14580"))
 PASSCODE = os.environ.get("PASSCODE", "-1")
 COMMAND = os.environ.get("COMMAND", "filter r/46.1/-122.9/50")
 INFO = os.environ.get("INFO", ":TEST     :this is only a test message")
-DUMP_UNKNOWN_PACKETS = os.environ.get("DUMP_UNKNOWN_PACKETS", "unknown.log")
-DUMP_KNOWN_PACKETS = os.environ.get("DUMP_KNOWN_PACKETS", "known.log")
-
-
-unknown_packets = open(DUMP_UNKNOWN_PACKETS, "a", encoding="utf-8")
-known_packets = open(DUMP_KNOWN_PACKETS, "a", encoding="utf-8")
 
 
 async def main():
-    transport, protocol = await tnc2.create_aprsis_connection(
-        host=HOST, port=PORT, user=MYCALL, passcode=PASSCODE, command=COMMAND
+    transport, protocol = await create_aprsis_connection(
+        host=HOST,
+        port=PORT,
+        user=MYCALL,
+        passcode=PASSCODE,
+        command=COMMAND,
     )
+    if int(PASSCODE) > 0:
+        protocol.write(
+            APRSFrame.ui(
+                destination=DESTINATION,
+                source=MYCALL,
+                info=InformationField.from_bytes(INFO.encode("ascii")),
+            )
+        )
+
     async for packet in protocol.read():
         print(str(packet))
-        try:
-            iframe = InformationField.from_bytes(packet.info)
-        except Exception as e:
-            print("\t{}\n\n".format(e))
-            unknown_packets.write("{}\n\t{}\n\n".format(packet, e))
-            continue
-        if type(iframe) is InformationField:
-            unknown_packets.write("{}\nNo decoder for {!r}\n\n".format(packet, iframe.data_type))
-        else:
-            known_packets.write("{}\n{!r}\n\n".format(packet, iframe))
-        print("\t{!r}\n\n".format(iframe))
+        print(repr(packet.info))
 
 
 if __name__ == "__main__":
     asyncio.run(main())
-
